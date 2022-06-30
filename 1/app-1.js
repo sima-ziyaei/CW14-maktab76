eda27fada737ad9319d1b27ccb139cb453067671
sima ziyaei <sziyaei2000@gmail.com>
2022-06-30 14:44:40 +0430

    cw14Q1

---
+++ 1/app-1.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("button").click(function(){
+      $("#div1").fadeToggle(3000);
+      $("#div2").fadeToggle(2000);
+      $("#div3").fadeToggle(1000);
+    });
+  });
